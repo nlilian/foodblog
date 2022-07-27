3a0c25ce4cdcec1aa66c94b8641682c15d8a01c1
nlilian <nnoromlili@gmail.com>
2022-07-27 22:18:18 +0100

    Header layout

---
+++ components/header.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-5">
+        <Link href="/">
+          <img
+            className="w-44 cursor-pointer object-contain"
+            src="https://links.papareact.com/yvf"
+            alt=""
+          />
+        </Link>
+
+        <div className="hidden md:inline-flex items-center space-x-5">
+          <h3>Categories</h3>
+          <h3>Recipes</h3>
+          <h3>Contact</h3>
+          <h3>About</h3>
+        </div>
+      </div>
+
+      <div className="flex items-center space-x-5 text-yellow-600"> 
+        <h3>Login</h3>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
